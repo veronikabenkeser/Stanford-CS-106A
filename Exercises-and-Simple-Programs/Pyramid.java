2d146711f47658f86fc7ae698c345840a04caeeb
Veronika Benkeser <veronika.benkeser@gmail.com>
2015-03-04 14:56:32 -0800

    Pyramid

--- Exercises-and-Simple-Programs/Pyramid.java
+++ Exercises-and-Simple-Programs/Pyramid.java
@@ -12,41 +12,36 @@
 
 import acm.graphics.*;
 import acm.program.*;
-
 import java.awt.*;
 
 public class Pyramid extends GraphicsProgram {
-
 	GRect brick = new GRect(BRICK_WIDTH, BRICK_HEIGHT);
-	
-/** Width of each brick in pixels */
 	private static final int BRICK_WIDTH = 30;
-
-/** Width of each brick in pixels */
 	private static final int BRICK_HEIGHT = 12;
-
-/** Number of bricks in the base of the pyramid */
 	private static final int BRICKS_IN_BASE = 14;
-	
 	private double ADJUSTED_BRICKS_IN_BASE = BRICKS_IN_BASE; //making it double so that the math works out later
-	
-	/* Pyramid Aligned to the Left
-	
+
 	public void run() {
-		for(int k=0; k<BRICKS_IN_BASE; k++){
+		drawPyramid();
+	}
+	
+	private void drawPyramid(){
 		
-			for(int i=0; i<=k; i++){
+		double startingPointx= getWidth()/2.0-ADJUSTED_BRICKS_IN_BASE*BRICK_WIDTH/2.0;
+		double startingPointY= getHeight()-BRICK_HEIGHT;
+		
+		for (int row=0; row<BRICKS_IN_BASE; row++){//do it 14 times and incr by1.
+			for (int column=0; column<ADJUSTED_BRICKS_IN_BASE; column++){
+			
+				GRect brick = new GRect(BRICK_WIDTH, BRICK_HEIGHT);
+				add(brick, startingPointx+BRICK_WIDTH*column, startingPointY-BRICK_HEIGHT*row); //e.d.in one roq, the 4th brick is 3 widths away, hencce why we use "column"
 				
-				int x=i*BRICK_WIDTH;
-				int y= k*BRICK_HEIGHT;
 				
-				GRect rectangle = new GRect(x, y, BRICK_WIDTH, BRICK_HEIGHT);
-				add(rectangle);
 			}
 			
-			System.out.println();
-			
-			
+			ADJUSTED_BRICKS_IN_BASE--;
+			startingPointx= getWidth()/2.0-ADJUSTED_BRICKS_IN_BASE*BRICK_WIDTH/2.0;
+
 		}
-  }
+	}
 }
