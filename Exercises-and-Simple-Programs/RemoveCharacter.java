2f0a21bbc8777abe7cef85b852f3f216833169c0
Veronika Benkeser <veronika.benkeser@gmail.com>
2015-03-04 15:05:16 -0800

    RemoveCharacter

---
+++ Exercises-and-Simple-Programs/RemoveCharacter.java
@@ -0,0 +1,22 @@
+import acm.program.ConsoleProgram;
+
+public class RemoveCharacter extends ConsoleProgram {
+	public void run() {
+		String str = readLine("Enter a sentence: ");
+		String  letterToRemove = readLine("Enter the letter you want to remove from the sentence above: ");
+		
+		char ch = letterToRemove.charAt(0);
+		 println(removeAllOccurances(str, ch));
+	}
+	
+	public String removeAllOccurances(String str, char ch){
+		String result="";
+		
+		for (int i =0; i<str.length(); i++){
+			if(str.charAt(i) != ch){			
+				result = result + str.charAt(i);
+			}		
+		}
+		return result;
+	}
+}
