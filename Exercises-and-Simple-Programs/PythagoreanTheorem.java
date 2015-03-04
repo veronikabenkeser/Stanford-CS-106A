4fba33b0e8525368a751a7f136deaac5aa9f2254
Veronika Benkeser <veronika.benkeser@gmail.com>
2015-03-04 15:00:17 -0800

    PythagoreanTheorem

---
+++ Exercises-and-Simple-Programs/PythagoreanTheorem.java
@@ -0,0 +1,14 @@
+import acm.program.ConsoleProgram;
+
+public class PythagoreanTheorem extends ConsoleProgram{ 
+
+	public void run(){
+		println("Enter values to compute Pythagorean theorem.");
+		int a = readInt("a: ");
+		int b = readInt("b: ");
+		
+		double c = (double)(Math.sqrt((Math.pow(a, 2))+Math.pow(b,2)));
+		
+		println("c = " + c);
+	}
+}
